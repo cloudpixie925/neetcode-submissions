f820fa28d70cc29265e49a10aa4bb11f2bd4bb30
cloudpixie925 <ganiyahayanlola2025@gmail.com>
2026-08-06 18:06:49 +0100

    Update submission-12.py

--- Data Structures & Algorithms/duplicate-integer/submission-12.py
+++ Data Structures & Algorithms/duplicate-integer/submission-12.py
@@ -1,14 +1,16 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+        
+        # This works but I just prefer using a hash set
         # nums.sort()
         # for i in range(1, len(nums)):
         #     if nums[i] == nums[i-1]:
         #         return True
         # return False
 
-        return len(nums) != len(set(nums))
 
-
+        # I liked it but it is not efficient enough
         # seen = {}
         # for i in range(len(nums)):
         #     if nums[i] in seen.values():
